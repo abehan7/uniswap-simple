d7e59e9512c4519e261c92f5240d8bb2d3fc69bc
unknown <hanjk13262@gmail.com>
2022-09-25 13:08:06 +0900

    chore

--- dist/test/__test__.js
+++ dist/test/__test__.js
@@ -12,4 +12,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("./data");
 const errorCase1 = data_1.testDepositParams.find((param) => param.name === "decimal truncation");
 const errorCase2 = data_1.testDepositParams.find((param) => param.name === "tiny minting amount #2");
-console.log(errorCase1);
+const normalCase = data_1.testDepositParams.find((param) => param.name === "decimal truncation #2");
+// console.log(normalCase);
+const params = [errorCase1, errorCase2, normalCase];
+const getDepositResult = (tc) => {
+    if (!normalCase)
+        return;
+    //   const tc = normalCase;
+    const ay = tc.y - 1;
+    // 아~ normal case같은 경우에는 1을 줄여버리니까 바로 코인 1개가 사라져버리네
+    const price = tc.rx / tc.ry;
+    const ax = ay * price;
+    const pc = tc.ps * (ax / tc.rx);
+    console.log(`ax: ${ax} ay: ${ay} pc: ${pc} `);
+};
+// 똑같이 1개 받을꺼면 더 줄여서 가져가라 이건가?
+const init = () => {
+    params.forEach((param) => getDepositResult(param));
+};
+init();
--- test/__test__.ts
+++ test/__test__.ts
@@ -18,4 +18,46 @@ const errorCase2 = testDepositParams.find(
   (param) => param.name === "tiny minting amount #2"
 );
 
-console.log(errorCase1);
+const normalCase = testDepositParams.find(
+  (param) => param.name === "decimal truncation #2"
+);
+
+// console.log(normalCase);
+
+const params = [errorCase1, errorCase2, normalCase];
+
+interface IParam {
+  name: string;
+  rx: number;
+  ry: number;
+
+  ps: number;
+
+  x: number;
+  y: number;
+
+  ax: number;
+  ay: number;
+
+  pc: number;
+}
+const getDepositResult = (tc: IParam) => {
+  if (!normalCase) return;
+  //   const tc = normalCase;
+  const ay = tc.y - 1;
+  // 아~ normal case같은 경우에는 1을 줄여버리니까 바로 코인 1개가 사라져버리네
+  const price = tc.rx / tc.ry;
+  const ax = ay * price;
+  const pc = tc.ps * (ax / tc.rx);
+  console.log(`ax: ${ax} ay: ${ay} pc: ${pc} `);
+};
+// 똑같이 1개 받을꺼면 더 줄여서 가져가라 이건가?
+
+const init = () => {
+  params.forEach((param) => getDepositResult(param!));
+};
+
+init();
+
+// 결론
+// pc갯수에 따라 ay가 달라진다
